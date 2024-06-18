ea744a75ce42180cd574877c5fe5ce7c5b758497
John Bakhmat <johnbakhmat@gmail.com>
2024-06-18 13:53:12 +0300

    reading dir

--- lib/library_manager.ml
+++ lib/library_manager.ml
@@ -1,13 +1,29 @@
 exception Dir_not_found
 
-let enter_dir(path:string) = 
+let enter_dir(path:string):unit = 
         match Sys.is_directory path with
         | false -> raise Dir_not_found;
         | true -> Sys.chdir path;
 ;;
+
+let process_file (path:string)=
+        [path]
+;;
+
+
+let rec walk_dir (path:string):string list = 
+        let dir_entries = Sys.readdir(path) in
+        let entries = Array.to_list(dir_entries) in
+        let process_entry (entry_name:string) = 
+                let entry = String.concat "/" [path;entry_name] in
+                if Sys.is_directory entry then walk_dir(entry) else process_file(entry) 
+        in
+        List.flatten (List.map process_entry entries)
+;;
                 
 let main () = 
-        print_endline (Sys.getcwd());
-        enter_dir "../";
-        print_endline (Sys.getcwd());
+        let dir = Sys.getcwd() in
+        let entries = walk_dir dir in
+        let res = String.concat "\n" entries in
+        print_endline res;
 ;;
